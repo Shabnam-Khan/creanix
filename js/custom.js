a5288c09a5770860dbde9228ae6cdfea614a1bb8
Shabnam-Khan <shabnam0648@gmail.com>
2021-05-11 01:00:24 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,85 @@
+$(function(){ 
+ //vertical slider
+ $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows:false,
+    vertical:true,
+    verticalSwiping:true,
+    centerMode:true,
+    centerPadding:false,
+  });
+
+// scroll spy 
+$(window).scroll(function(){
+  var scrolling = $(this).scrollTop();
+  if(scrolling > 300){
+    $('.navbar').addClass('navbg');
+  }
+  else{
+    $('.navbar').removeClass('navbg');
+  }
+});
+
+//animation scroll js
+var html_body = $('html, body');
+$('nav a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+        html_body.animate({
+        scrollTop: target.offset().top - 95
+        }, 1500);
+        return false;
+        }
+      }
+  });
+
+// counter
+  $('.counter').counterUp({
+      delay: 5,
+      time: 1000
+  });
+
+//  back to top 
+$(window).scroll(function(){
+  var scrolling = $(this).scrollTop();
+  if(scrolling > 100){
+    $('.back-to-top').fadeIn(500);
+ }
+  else{
+    $('.back-to-top').fadeOut(500);
+  }
+});
+
+// venobox
+$('.venobox').venobox(); 
+
+//team wow js
+new WOW().init();
+
+// testimonial slider 
+$('.testimonial-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows:false,
+  fade:true,
+  speed:2500,
+});
+
+ // marketplace slider 
+ $('.market-slide').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows:false,
+});
+});
+
+
