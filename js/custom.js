ff4cb34470e5b2bf12285bbd42458409b2c7bc72
Shabnam-Khan <shabnam0648@gmail.com>
2021-06-08 16:54:17 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,35 @@
 $(function(){ 
+
+// navbar toggle 
+$('.navbar-toggler i').click(function(){
+  $('.fa-close').toggleClass('fa-bars')
+});
+
  //vertical slider
  $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2500,
     arrows:false,
     vertical:true,
     verticalSwiping:true,
     centerMode:true,
     centerPadding:false,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 
 // scroll spy 
@@ -70,6 +90,20 @@ $('.testimonials').slick({
   arrows:false,
   speed:2500,
   dots:true,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
 
  // marketplace slider 
@@ -79,117 +113,131 @@ $('.testimonials').slick({
   autoplay: true,
   autoplaySpeed: 1500,
   arrows:false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
 });
 
 
 // contact form validation 
-var firstname = document.getElementById('firstname');
-var firstname_error = document.getElementById('firstname_error');
+// var firstname = document.getElementById('firstname');
+// var firstname_error = document.getElementById('firstname_error');
 
-var lastname = document.getElementById('lastname');
-var lastname_error = document.getElementById('lastname_error');
+// var lastname = document.getElementById('lastname');
+// var lastname_error = document.getElementById('lastname_error');
 
-var email = document.getElementById('email');
-var email_error = document.getElementById('email_error');
-var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// var email = document.getElementById('email');
+// var email_error = document.getElementById('email_error');
+// var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-var phone = document.getElementById('phone');
-var phone_error = document.getElementById('phone_error');
-var phoneregex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+// var phone = document.getElementById('phone');
+// var phone_error = document.getElementById('phone_error');
+// var phoneregex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
-var subject = document.getElementById('subject');
-var subject_error = document.getElementById('subject_error');
+// var subject = document.getElementById('subject');
+// var subject_error = document.getElementById('subject_error');
 
-var message = document.getElementById('message');
-var message_error = document.getElementById('message_error');
+// var message = document.getElementById('message');
+// var message_error = document.getElementById('message_error');
 
 
-function sub(){
-  if(firstname.value == ''){
-      firstname_error.innerHTML = "Enter your first name";
-      firstname.style.border = '2px solid red';
-      firstname.focus();
-      return false;
-  }
-  if(lastname.value == ''){
-      lastname_error.innerHTML = "Enter your last name";
-      lastname.style.border = '2px solid red';
-      lastname.focus();
-      return false;
-  }
-  if(email.value == ''){
-      email_error.innerHTML = "Enter your email address";
-      email.style.border = '2px solid red';
-      email.focus();
-      return false;
-  }
-  if(!regex.test(email.value)){
-      email_error.innerHTML = "Enter a valid email address";
-      email.style.border = '2px solid red';
-      email.focus();
-      return false;
-  }
-  if(phone.value == ''){
-      phone_error.innerHTML = "Enter your phone number";
-      phone.style.border = '2px solid red';
-      phone.focus();
-      return false;
-  }
-  if(!phoneregex.test(phone.value)){
-      phone_error.innerHTML = "Enter a 10-digit phone number";
-      phone.style.border = '2px solid red';
-      phone.focus();
-      return false;
-  }
-  if(subject.value == ''){
-    subject_error.innerHTML = "Enter your subject";
-    subject.style.border = '2px solid red';
-    subject.focus();
-    return false;
-}
-  if(message.value == ''){
-    message_error.innerHTML = "Enter your message";
-    message.style.border = '2px solid red';
-    message.focus();
-    return false;
-}
+// function sub(){
+//   if(firstname.value == ''){
+//       firstname_error.innerHTML = "Enter your first name";
+//       firstname.style.border = '2px solid red';
+//       firstname.focus();
+//       return false;
+//   }
+//   if(lastname.value == ''){
+//       lastname_error.innerHTML = "Enter your last name";
+//       lastname.style.border = '2px solid red';
+//       lastname.focus();
+//       return false;
+//   }
+//   if(email.value == ''){
+//       email_error.innerHTML = "Enter your email address";
+//       email.style.border = '2px solid red';
+//       email.focus();
+//       return false;
+//   }
+//   if(!regex.test(email.value)){
+//       email_error.innerHTML = "Enter a valid email address";
+//       email.style.border = '2px solid red';
+//       email.focus();
+//       return false;
+//   }
+//   if(phone.value == ''){
+//       phone_error.innerHTML = "Enter your phone number";
+//       phone.style.border = '2px solid red';
+//       phone.focus();
+//       return false;
+//   }
+//   if(!phoneregex.test(phone.value)){
+//       phone_error.innerHTML = "Enter a 10-digit phone number";
+//       phone.style.border = '2px solid red';
+//       phone.focus();
+//       return false;
+//   }
+//   if(subject.value == ''){
+//     subject_error.innerHTML = "Enter your subject";
+//     subject.style.border = '2px solid red';
+//     subject.focus();
+//     return false;
+// }
+//   if(message.value == ''){
+//     message_error.innerHTML = "Enter your message";
+//     message.style.border = '2px solid red';
+//     message.focus();
+//     return false;
+// }
  
-}
+// }
 
-function errorvalidate(){
-  if(firstname.value != ''){
-      firstname_error.innerHTML = "";
-      firstname.style.border = '2px solid green';
-  }
-  if(lastname.value != ''){
-      lastname_error.innerHTML = "";
-      lastname.style.border = '2px solid green';
-  }
-  if(email.value != ''){
-      email_error.innerHTML = "";
-      email.style.border = '2px solid green';
-  }
-  if(phone.value != ''){
-      phone_error.innerHTML = "";
-      phone.style.border = '2px solid green';
-  }
-  if(subject.value != ''){
-    subject_error.innerHTML = "";
-    subject.style.border = '2px solid green';
-}
-  if(message.value != ''){
-    message_error.innerHTML = "";
-    message.style.border = '2px solid green';
-}
-}
+// function errorvalidate(){
+//   if(firstname.value != ''){
+//       firstname_error.innerHTML = "";
+//       firstname.style.border = '2px solid green';
+//   }
+//   if(lastname.value != ''){
+//       lastname_error.innerHTML = "";
+//       lastname.style.border = '2px solid green';
+//   }
+//   if(email.value != ''){
+//       email_error.innerHTML = "";
+//       email.style.border = '2px solid green';
+//   }
+//   if(phone.value != ''){
+//       phone_error.innerHTML = "";
+//       phone.style.border = '2px solid green';
+//   }
+//   if(subject.value != ''){
+//     subject_error.innerHTML = "";
+//     subject.style.border = '2px solid green';
+// }
+//   if(message.value != ''){
+//     message_error.innerHTML = "";
+//     message.style.border = '2px solid green';
+// }
+// }
 
-firstname.addEventListener('blur', errorvalidate);
-lastname.addEventListener('blur', errorvalidate);
-email.addEventListener('blur', errorvalidate);
-phone.addEventListener('blur', errorvalidate);
-subject.addEventListener('blur', errorvalidate);
-message.addEventListener('blur', errorvalidate);
+// firstname.addEventListener('blur', errorvalidate);
+// lastname.addEventListener('blur', errorvalidate);
+// email.addEventListener('blur', errorvalidate);
+// phone.addEventListener('blur', errorvalidate);
+// subject.addEventListener('blur', errorvalidate);
+// message.addEventListener('blur', errorvalidate);
 
 // newsletter email validation
 
@@ -218,5 +266,4 @@ function errvalidate(){
         newsemail.style.border = '2px solid green';
     }
 }
-
 newsemail.addEventListener('blur', errvalidate);
